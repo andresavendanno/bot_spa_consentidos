6231b8d3f78637a8ec07591fb8c1c9324caaf26c
Zipa2309 <andresavendanno@hotmail.com>
2025-06-10 17:09:39 -0500

    Funciona seleccion servicios, prueba de turnos para visualizar3

--- api/exportar_turnos.php
+++ api/exportar_turnos.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once 'config/conexion.php'; // Ruta a tu archivo de conexión
 
 // 🔐 Clave de seguridad
