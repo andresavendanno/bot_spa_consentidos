dad07b78a7b808c2963bf07490124ea93e1b1115
Zipa2309 <andresavendanno@hotmail.com>
2025-11-03 02:09:54 -0300

    retomando bot nuevo token y nuevo whatsapp url

--- config/constantes.php
+++ config/constantes.php
@@ -2,5 +2,5 @@
 const TOKEN_SPACONSENTIDOS = "CONSENTIDOSPORMAYMETA";
 const WEBHOOK_URL = "whatsappapi.spaconsentidos.website/webhook.php";
 const WHATSAPP_TOKEN = "EAAUZAHdaMZB7sBPy6NNEbpFjc2hB4ZBksgfNv8ZCMKYBvC3ctcAehNKlvgZBBLK4hOFuksdj6VEgxBpsmrHmTbQmpwtGIiZAZBoOhckqjL1GdiRCmOkdniilK1tJk5ZCzWjZBqbc9pcZCLqtRIlkVkqZCU2gImGziBFyQdXfLCfLQXF8dI2DPcplVzimH7wbFUilgJtZCAZDZD";
-const WHATSAPP_URL = "https://graph.facebook.com/v22.0/646389751893147/messages";
+const WHATSAPP_URL = "https://graph.facebook.com/v22.0/813946905141918/messages";
 const TU_CLAVE = "CONSENTIDOSPORMAmeta05";
