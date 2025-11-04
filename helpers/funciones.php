0f5aa03d79879b92c209a8145c10a00cafa92d95
Zipa2309 <andresavendanno@hotmail.com>
2025-11-04 18:12:47 -0300

    probado con +57 y actualizando +54 v.4

--- helpers/funciones.php
+++ helpers/funciones.php
@@ -12,7 +12,7 @@ function ajustarNumeroSandbox($numero) {
         if (preg_match('/^54911\d{8}$/', $numero)) {
             // Si NO tiene 15, lo agregamos: +54911 -> +5491115
             if (!preg_match('/^5491115\d{8}$/', $numero)) {
-                return substr($numero, 0, 7) . '15' . substr($numero, 7);
+                return substr($numero, 0, 5) . '15' . substr($numero, 5);
             }
         }
     }
